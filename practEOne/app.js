970802e53f0e2cc396bdf2dbe2c433b44c27baab
Ronald <rdajila@gmail.com>
2019-05-13 21:00:49 +0200

    Changes

--- practEOne/app.js
+++ practEOne/app.js
@@ -13,11 +13,11 @@ app.get("/", function(req, res){
 
 var Web3 = require("web3");
 
-web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));	
+web3 = new Web3(new Web3.providers.HttpProvider("http://192.168.32.139:8545"));	
 
 //var proofContract = web3.eth.contract([{"constant":false,"inputs":[{"name":"fileHash","type":"string"}],"name":"get","outputs":[{"name":"timestamp","type":"uint256"},{"name":"owner","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"owner","type":"string"},{"name":"fileHash","type":"string"}],"name":"set","outputs":[],"payable":false,"type":"function"},{"anonymous":false,"inputs":[{"indexed":false,"name":"status","type":"bool"},{"indexed":false,"name":"timestamp","type":"uint256"},{"indexed":false,"name":"owner","type":"string"},{"indexed":false,"name":"fileHash","type":"string"}],"name":"logFileAddedStatus","type":"event"}]);
 var proofContract = web3.eth.contract([{"constant":true,"inputs":[{"name":"fileHash","type":"string"}],"name":"get","outputs":[{"name":"timestamp","type":"uint256"},{"name":"owner","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"owner","type":"string"},{"name":"fileHash","type":"string"}],"name":"set","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"anonymous":false,"inputs":[{"indexed":false,"name":"status","type":"bool"},{"indexed":false,"name":"timestamp","type":"uint256"},{"indexed":false,"name":"owner","type":"string"},{"indexed":false,"name":"fileHash","type":"string"}],"name":"logFileAddedStatus","type":"event"}]);
-var proof = proofContract.at("0x832717746cb408f857613d49e2a615a6af2a6f34");
+var proof = proofContract.at("0x0b72b8964a8619ef8439d7042769c7b927aa7c02");
 
 app.get("/submit", function(req, res){
 	var fileHash = req.query.hash;
